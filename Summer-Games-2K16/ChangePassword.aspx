d2ea7c278a6cef47b28e90d44803afbc9f8ba299
Ebin Antoo <ebin@ebin-antoo.com>
2016-06-22 12:47:14 -0400

    Added change password, but password hash issue

---
+++ Summer-Games-2K16/ChangePassword.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FrontEnd.Master" AutoEventWireup="true" CodeBehind="ChangePassword.aspx.cs" Inherits="Summer_Games_2K16.ChangePassword" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+  
+           <asp:TextBox ID="txtNewPassword" TextMode="Password" 
+            runat="server"></asp:TextBox>
+
+  
+            <p>Confirm New Password</p>
+        
+            <asp:TextBox ID="txtConfirmNewPassword" TextMode="Password" runat="server">
+            </asp:TextBox>
+
+
+
+            &nbsp;<asp:Button ID="btnSave" runat="server" 
+            Text="Save" OnClick="btnSave_Click" Width="70px" />
+
+            <asp:Label ID="lblMessage" runat="server">
+            </asp:Label>
+
+            <asp:ValidationSummary ID="ValidationSummary1" 
+            ForeColor="Red" runat="server" />
+
+
+</asp:Content>
---
+++ Summer-Games-2K16/ResetPassword.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Net.Mail;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Summer_Games_2K16
+{
+    public partial class ResetPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnResetPassword_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString1"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("spResetPassword", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramUsername = new SqlParameter("@UserName", txtUserName.Text);
+
+                cmd.Parameters.Add(paramUsername);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    if (Convert.ToBoolean(rdr["ReturnCode"]))
+                    {
+                        SendPasswordResetEmail(rdr["Email"].ToString(), txtUserName.Text, rdr["UniqueId"].ToString());
+                        lblMessage.Text = "An email with instructions to reset your password is sent to your registered email";
+                    }
+                    else
+                    {
+                        lblMessage.ForeColor = System.Drawing.Color.Red;
+                        lblMessage.Text = "Username not found!";
+                    }
+                }
+            }
+        }
+
+        private void SendPasswordResetEmail(string ToEmail, string UserName, string UniqueId)
+        {
+            // MailMessage class is present is System.Net.Mail namespace
+            MailMessage mailMessage = new MailMessage("YourEmail@gmail.com", ToEmail);
+
+
+            // StringBuilder class is present in System.Text namespace
+            StringBuilder sbEmailBody = new StringBuilder();
+            sbEmailBody.Append("Dear " + UserName + ",<br/><br/>");
+            sbEmailBody.Append("Please click on the following link to reset your password");
+            sbEmailBody.Append("<br/>"); sbEmailBody.Append("http://localhost/ChangePassword.aspx?uid=" + UniqueId);
+            sbEmailBody.Append("<br/><br/>");
+            sbEmailBody.Append("<b>Pragim Technologies</b>");
+
+            mailMessage.IsBodyHtml = true;
+
+            mailMessage.Body = sbEmailBody.ToString();
+            mailMessage.Subject = "Reset Your Password";
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            smtpClient.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "summergames2k16@gmail.com",
+                Password = "games2k16"
+            };
+
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
